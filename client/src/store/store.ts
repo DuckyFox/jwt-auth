0a99fb7eba1a85bf4de328c577985d47ae2d7fa7
DuckyFox <108066933+DuckyFox@users.noreply.github.com>
2025-12-19 23:20:51 +0400

    created zustand store

---
+++ client/src/store/store.ts
@@ -0,0 +1,47 @@
+import { create } from 'zustand'
+import {UserInterface} from "@/models/UserInterface";
+import AuthService from "@/services/AuthService";
+import { AxiosError } from 'axios';
+
+export const useUser = create((set) => ({
+    user: {} as UserInterface,
+    isAuth: false,
+    setAuth: (bool: boolean) => set({isAuth: bool}),
+    setUser: (user: UserInterface) => set({user}),
+    login: async (email: string, password: string) => {
+        try {
+            const response = await AuthService.login(email, password)
+            localStorage.setItem('token', response.data.accessToken)
+            set({isAuth: true})
+            set({user: response.data.user})
+        } catch (e: unknown) {
+            if (e instanceof AxiosError) {
+                console.log(e.response?.data?.message);
+            }
+        }
+    },
+    register: async (email: string, password: string) => {
+        try {
+            const response = await AuthService.registration(email, password)
+            localStorage.setItem('token', response.data.accessToken)
+            set({isAuth: true})
+            set({user: response.data.user})
+        } catch (e: unknown) {
+            if (e instanceof AxiosError) {
+                console.log(e.response?.data?.message);
+            }
+        }
+    },
+    logout: async () => {
+        try {
+            const response = await AuthService.logout()
+            localStorage.removeItem('token')
+            set({isAuth: false})
+            set({} as UserInterface)
+        } catch (e: unknown) {
+            if (e instanceof AxiosError) {
+                console.log(e.response?.data?.message);
+            }
+        }
+    },
+}))
